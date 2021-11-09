6232533c56a7b4dc5b423833c15b0f16075bd1c1
osboxes.org <jdelansorne@gmail.com>
2021-11-09 18:56:06 +0000

    script B fini

--- script-b.py
+++ script-b.py
@@ -8,7 +8,8 @@ contentList = content.replace('\n', ' ').split(" ")
 for i in range (1,len(contentList)):
     if i % 2 != 0:
         ips.append(contentList[i])
-print(ips)
-#r = requests.get('https://api.github.com/user')
+for i in ips:
+    r = requests.get('http://%s:80' % i)
+    print('IP: %s Response: %s' % (i,r))
 
 #print('IP: %s Response: %s ' %(,r.status_code))
