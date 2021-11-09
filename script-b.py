2c27cf5079bef668628634c70c69d77f80bd190c
osboxes.org <jdelansorne@gmail.com>
2021-11-09 18:59:19 +0000

    script A et B fini

--- script-b.py
+++ script-b.py
@@ -1,5 +1,4 @@
 import requests
-import csv
 
 ips=[]
 f = open('vm-infos.txt', 'r')
@@ -11,5 +10,3 @@ for i in range (1,len(contentList)):
 for i in ips:
     r = requests.get('http://%s:80' % i)
     print('IP: %s Response: %s' % (i,r))
-
-#print('IP: %s Response: %s ' %(,r.status_code))
--- script-c.py
+++ script-c.py
@@ -2,3 +2,11 @@ import boto3
 
 ec2_resource = boto3.resource('ec2')
 ec2_client = boto3.client('ec2')
+
+ips=[]
+f = open('vm-infos.txt', 'r')
+content = f.read()
+contentList = content.replace('\n', ' ').split(" ")
+for i in range (1,len(contentList)):
+    if i % 2 != 0:
+        ips.append(contentList[i])
